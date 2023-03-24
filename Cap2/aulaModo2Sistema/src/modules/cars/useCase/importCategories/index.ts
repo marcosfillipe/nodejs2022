823d173405f25a663cff161d08e7532b66129f8f
Fillipe <marcosfillipebezerra@gmail.com>
2023-03-23 22:47:41 -0300

    iniciando modulo 6 cap2

--- Cap2/aulaModo2Sistema/src/modules/cars/useCase/importCategories/index.ts
+++ Cap2/aulaModo2Sistema/src/modules/cars/useCase/importCategories/index.ts
@@ -1,7 +1,13 @@
+import { CategoriesRepository } from "../../repositories/implemetations/CategoriesRepository";
 import { ImportCategoriesController } from "./ImportCategoriesController";
+import { ImportCategoriesService } from "./ImportCategoriesService";
 
-
-const importCategoriesController = new ImportCategoriesController();
-
+const categoriesRepository = CategoriesRepository.getInstance();
+const importCategoriesService = new ImportCategoriesService(
+  categoriesRepository
+);
+const importCategoriesController = new ImportCategoriesController(
+  importCategoriesService
+);
 
 export { importCategoriesController };
