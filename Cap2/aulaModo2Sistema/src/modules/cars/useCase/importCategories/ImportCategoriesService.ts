68866023088c166e59b2d137986c920d07a6fac8
Fillipe <marcosfillipebezerra@gmail.com>
2023-03-25 10:25:52 -0300

    termino cap2

--- Cap2/aulaModo2Sistema/src/modules/cars/useCase/importCategories/ImportCategoriesService.ts
+++ Cap2/aulaModo2Sistema/src/modules/cars/useCase/importCategories/ImportCategoriesService.ts
@@ -28,6 +28,7 @@ class ImportCategoriesService {
           });
         })
         .on("end", () => {
+          fs.promises.unlink(file.path);
           resolve(categories);
         })
         .on("error", (err) => {
