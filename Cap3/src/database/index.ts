19f27948fffa1139d51dc36b3b241d815c333e48
Fillipe <marcosfillipebezerra@gmail.com>
2023-03-27 22:56:53 -0300

    alteracoes docker

--- Cap3/src/database/index.ts
+++ Cap3/src/database/index.ts
@@ -1,19 +1,3 @@
-import { DataSource } from "typeorm";
+import { createConnection } from "typeorm";
 
-const appDataSource = new DataSource({
-  type: "postgres",
-  host: "db_rentacar",
-  port: 5444,
-  username: "docker",
-  password: "teste",
-  database: "db_rentacar",
-});
-
-appDataSource
-  .initialize()
-  .then(() => {
-    console.log("Data Source has been initialized!");
-  })
-  .catch((err) => {
-    console.error("Error during Data Source initialization", err);
-  });
+createConnection();
